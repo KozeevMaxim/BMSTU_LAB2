9c9314dbf98d037567107813a46363334c4088d5
KozeevMaxim <maxim4244@gmail.com>
2016-10-14 14:29:17 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+#include "calculator.hpp"
+int main() {
+    float a, b;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << sum(a, b) << std::endl;
+    std::cout << dev(a, b) << std::endl;
+    std::cout << mult(a, b) << std::endl;
+    std::cout << sub(a, b) << std::endl;
+    std::cout << pow(a, b) << std::endl;
+    std::cout << sqrt(a) << std::endl;
+}
